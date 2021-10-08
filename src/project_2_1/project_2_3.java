37f9ba2de19eca1161fd0c0f9a84ca78fc63e9f6
happyblueflower <3120661989@qq.com>
2021-10-08 19:54:09 +0800

    6

---
+++ src/project_2_1/project_2_3.java
@@ -0,0 +1,20 @@
+package project_2_1;
+import java.util.Scanner;
+
+public class project_2_3 {
+	public static void main(String[] args) {
+		//@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter three numbers: ");
+		
+		double number1 = input.nextDouble();
+		double number2 = input.nextDouble();
+		double number3 = input.nextDouble();
+		
+		double average = (number1 + number2 + number3) / 3;
+		System.out.print("The average of the three number is " + average);
+	}
+
+}
+//input 为什么会出现警告
